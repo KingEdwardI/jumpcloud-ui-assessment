2024cfb4200fb55e009c07d6d24c83e3ab561630
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-12-21 17:29:29 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container text-xs-center>
+        <p class="display-4"> Hello JumpCloud </p>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: { }
+}
+</script>
+
+<style>
+* {
+  font-family: 'Exo', sans-serif;
+}
+
+</style>

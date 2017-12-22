af1723b62009f06afa9a580a28aa51a118504a49
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-12-21 18:46:15 -0700

    AddTodo working

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,10 +1,47 @@
 <template>
-  <p> add-todo </p>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 text-xs-center>
+        <v-form v-on:submit="createTodo">
+          <v-text-field
+            label="Description"
+            v-model="description"
+            required
+          ></v-text-field>
+          <v-btn type="submit">Create</v-btn>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import config from '../../config'
 export default {
-  name: 'add-todo'
+  name: 'add-todo',
+  data () {
+    return {
+      description: ''
+    }
+  },
+  methods: {
+    createTodo: function (evt) {
+      evt.preventDefault()
+      let data = {
+        description: this.description
+      }
+      let url = config.api_url + '/api/todos'
+      let params = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }
+      fetch(url, params).then((res) => res.text()).then((data) => console.log(data))
+    }
+  }
 }
 </script>
 

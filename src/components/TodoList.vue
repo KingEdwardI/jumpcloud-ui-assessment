26271a81d57f1a288140155203062637be8dcf04
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-12-21 18:47:10 -0700

    TodoList displaying from server

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,39 @@
 <template>
-  <p>todo-list</p>
+  <v-container>
+    <v-layout row v-for="todo in todos" :key="JSON.stringify(todo)">
+      <v-flex xs11>
+        <v-list-tile-title v-text="todo.description"/>
+      </v-flex>
+      <v-flex xs1>
+        <v-checkbox v-model="todo.done"/>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import config from '../../config'
+
 export default {
-  name: 'todo-list'
+  name: 'todo-list',
+  data () {
+    return {
+      todos: []
+    }
+  },
+  created () {
+    this.getTodos()
+  },
+  methods: {
+    getTodos: function () {
+      let url = config.api_url + '/api/todos'
+      try {
+        fetch(url).then((res) => res.json()).then((data) => { this.todos = data })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 }
 </script>
 
